9cfb177b104ee63af368a3af7515d90bfd43ba58
jugonzal <jugonzal@gmail.com>
2019-07-09 22:29:52 -0400

    First few resolvers for Categories

--- src/start.js
+++ src/start.js
@@ -4,8 +4,6 @@ import bodyParser from 'body-parser'
 import {graphqlExpress, graphiqlExpress} from 'graphql-server-express'
 import {makeExecutableSchema} from 'graphql-tools'
 import cors from 'cors'
-import {prepare} from "../util/index"
-
 
 const app = express()
 
@@ -14,7 +12,7 @@ app.use(cors())
 const homePath = '/graphiql'
 const URL = 'http://localhost'
 const PORT = 3001
-const MONGO_URL = 'mongodb://localhost:27017/blog'
+const MONGO_URL = 'mongodb://localhost:27017/coe'
 
 
 
@@ -22,74 +20,83 @@ export const start = async () => {
   try {
     const db = await MongoClient.connect(MONGO_URL)
 
-    const Posts = db.collection('posts')
-    const Comments = db.collection('comments')
-
     const typeDefs = [`
       type Query {
-        post(_id: String): Post
-        posts: [Post]
-        comment(_id: String): Comment
+        assessments: [Assessment]
       }
 
-      type Post {
-        _id: String
+      type Assessment {
+        code: String
         title: String
-        content: String
-        comments: [Comment]
+        categories: [Category]
       }
 
-      type Comment {
-        _id: String
-        postId: String
-        content: String
-        post: Post
-      }
-
-      type Mutation {
-        createPost(title: String, content: String): Post
-        createComment(postId: String, content: String): Comment
+      type Category {
+        code: String
+        label: String
+        assessment: Assessment
       }
 
       schema {
-        query: Query
-        mutation: Mutation
+        query: Query 
       }
-    `];
+    `]
 
     const resolvers = {
       Query: {
-        post: async (root, {_id}) => {
-          return prepare(await Posts.findOne(ObjectId(_id)))
-        },
-        posts: async () => {
-          return (await Posts.find({}).toArray()).map(prepare)
-        },
-        comment: async (root, {_id}) => {
-          return prepare(await Comments.findOne(ObjectId(_id)))
-        },
-      },
-      Post: {
-        comments: async ({_id}) => {
-          return (await Comments.find({postId: _id}).toArray()).map(prepare)
+        assessments: async () => {
+          return (await db.collection('assessments').find({},{code:1, title:1}).toArray())
         }
       },
-      Comment: {
-        post: async ({postId}) => {
-          return prepare(await Posts.findOne(ObjectId(postId)))
+      Assessment: {
+        categories: async ({code}) => {
+          return (await db.collection('categories').find({assessment_code: code},{code:1, label:1}).toArray())
         }
       },
-      Mutation: {
-        createPost: async (root, args, context, info) => {
-          const res = await Posts.insertOne(args)
-          return prepare(res.ops[0])  // https://mongodb.github.io/node-mongodb-native/3.1/api/Collection.html#~insertOneWriteOpResult
-        },
-        createComment: async (root, args) => {
-          const res = await Comments.insert(args)
-          return prepare(await Comments.findOne({_id: res.insertedIds[1]}))
-        },
-      },
+      Category: {
+        assessment: async ({code}) => {
+          let category = await db.collection('categories').findOne({code: code},{assessment_code:1})
+          console.log('about to look for assessment ', category.assessment_code)
+          let assessment = await db.collection('assessments').findOne({code: category.assessment_code},{code:1, title:1})
+          console.log('found ', assessment)
+          return assessment
+        }
+      }
     }
+
+    // const oldResolvers = {
+    //   Query: {
+    //     post: async (root, {_id}) => {
+    //       return prepare(await Posts.findOne(ObjectId(_id)))
+    //     },
+    //     posts: async () => {
+    //       return (await Posts.find({}).toArray()).map(prepare)
+    //     },
+    //     comment: async (root, {_id}) => {
+    //       return prepare(await Comments.findOne(ObjectId(_id)))
+    //     },
+    //   },
+    //   Post: {
+    //     comments: async ({_id}) => {
+    //       return (await Comments.find({postId: _id}).toArray()).map(prepare)
+    //     }
+    //   },
+    //   Comment: {
+    //     post: async ({postId}) => {
+    //       return prepare(await Posts.findOne(ObjectId(postId)))
+    //     }
+    //   },
+    //   Mutation: {
+    //     createPost: async (root, args, context, info) => {
+    //       const res = await Posts.insertOne(args)
+    //       return prepare(res.ops[0])  // https://mongodb.github.io/node-mongodb-native/3.1/api/Collection.html#~insertOneWriteOpResult
+    //     },
+    //     createComment: async (root, args) => {
+    //       const res = await Comments.insert(args)
+    //       return prepare(await Comments.findOne({_id: res.insertedIds[1]}))
+    //     },
+    //   },
+    // }
 
     const schema = makeExecutableSchema({
       typeDefs,
